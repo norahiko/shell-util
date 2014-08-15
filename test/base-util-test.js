e61f740beaf2ef7fdc189b99e92b41e411a0eda1
norahiko <norahiko.o@gmail.com>
2014-08-15 17:12:08 +0900

    Change assertion library

--- test/base-util-test.js
+++ test/base-util-test.js
@@ -2,8 +2,9 @@
 
 var sh = require("../lib/index.js");
 
-var assert = require("chai").assert;
-var equal = assert.strictEqual;
+//var assert = require("chai").assert;
+
+var assert = require("power-assert");
 var env = sh.env;
 
 suite("Expand tool:", function () {
@@ -23,62 +24,62 @@ suite("Expand tool:", function () {
     });
 
     test("expand env", function () {
-        equal(sh.expand("$key"), "value");
-        equal(sh.expand("${key}"), "value");
-        equal(sh.expand("/a/$key/b"), "/a/value/b");
-        equal(sh.expand("$cwd"), "/test");
-        equal(sh.expand("$file"), "dir/file.js");
-        equal(sh.expand("~"), process.env.HOME);
+        assert(sh.expand("$key") === "value");
+        assert(sh.expand("${key}") === "value");
+        assert(sh.expand("/a/$key/b") === "/a/value/b");
+        assert(sh.expand("$cwd") === "/test");
+        assert(sh.expand("$file") === "dir/file.js");
+        assert(sh.expand("~") === process.env.HOME);
     });
 
     test("expand attributes", function() {
-        equal(sh.expand("$file.length"), "11");
-        equal(sh.expand("${file.length}"), "11");
-        equal(sh.expand("$object.key.2.0"), "piyo");
-        equal(sh.expand("${object.path:abs}"), "/test/path.obj");
+        assert(sh.expand("$file.length") === "11");
+        assert(sh.expand("${file.length}") === "11");
+        assert(sh.expand("$object.key.2.0") === "piyo");
+        assert(sh.expand("${object.path:abs}") === "/test/path.obj");
     });
 
     test("expand filter", function() {
         if(process.platform !== "win32") {
-            equal(sh.expand("$file:abs"), "/test/dir/file.js");
-            equal(sh.expand("${file:abs}"), "/test/dir/file.js");
+            assert(sh.expand("$file:abs") === "/test/dir/file.js");
+            assert(sh.expand("${file:abs}") === "/test/dir/file.js");
         }
-        equal(sh.expand("$file:dir"), "dir");
-        equal(sh.expand("$file:base"), "file.js");
-        equal(sh.expand("$file:rmext"), "dir/file");
+        assert(sh.expand("$file:dir") === "dir");
+        assert(sh.expand("$file:base") === "file.js");
+        assert(sh.expand("$file:rmext") === "dir/file");
 
         env.num = 0;
-        equal(sh.expand("$num:digit2"), "00");
+        assert(sh.expand("$num:digit2") === "00");
 
         env.num = 1;
-        equal(sh.expand("$num:digit2"), "01");
+        assert(sh.expand("$num:digit2") === "01");
 
         env.num = -1;
-        equal(sh.expand("$num:digit2"), "-01");
+        assert(sh.expand("$num:digit2") === "-01");
 
         env.num = 1.234;
-        equal(sh.expand("$num:digit2"), "01");
+        assert(sh.expand("$num:digit2") === "01");
 
         env.num = 1.9;
-        equal(sh.expand("$num:digit2"), "01");
+        assert(sh.expand("$num:digit2") === "01");
 
         env.num = -1.9;
-        equal(sh.expand("$num:digit2"), "-01");
+        assert(sh.expand("$num:digit2") === "-01");
 
         env.num = 10;
-        equal(sh.expand("$num:digit2"), "10");
+        assert(sh.expand("$num:digit2") === "10");
 
         env.num = -10;
-        equal(sh.expand("$num:digit2"), "-10");
+        assert(sh.expand("$num:digit2") === "-10");
 
         env.num = 100;
-        equal(sh.expand("$num:digit2"), "100");
+        assert(sh.expand("$num:digit2") === "100");
 
         env.num = -100;
-        equal(sh.expand("$num:digit2"), "-100");
+        assert(sh.expand("$num:digit2") === "-100");
 
         env.num = 123;
-        equal(sh.expand("$num:digit6"), "000123");
+        assert(sh.expand("$num:digit6") === "000123");
     });
 
     test("new filter", function() {
@@ -86,16 +87,16 @@ suite("Expand tool:", function () {
             return str.toUpperCase();
         };
         env.text = "text";
-        equal(sh.expand("$text:upper"), "TEXT");
+        assert(sh.expand("$text:upper") === "TEXT");
     });
 
     test("not expanded", function () {
-        equal(sh.expand("${key"), "${key");
+        assert(sh.expand("${key") === "${key");
     });
 
     test("expand array", function() {
         env.ary = ["foo", "bar", "baz"];
-        equal(sh.expand("$ary"), "foo bar baz");
+        assert(sh.expand("$ary") === "foo bar baz");
     });
 
     test("expand error", function() {
