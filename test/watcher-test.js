151e843ab7465541ccfa23a4f2e46c23592d6e60
norahiko <norahiko.o@gmail.com>
2014-10-05 00:12:54 +0900

    Improve watcher

--- test/watcher-test.js
+++ test/watcher-test.js
@@ -2,17 +2,18 @@
 
 var sh = require("../lib/");
 var assert = require("power-assert");
-var pathModule = require("path");
 var fs = require("fs");
 
 suite("watcher", function() {
     var root = process.cwd();
 
     setup(function () {
+        sh.remove("test/data");
         sh.mkdir("test/data");
         process.chdir("test/data");
         fs.writeFileSync("one.css", "one");
         fs.writeFileSync("two.css", "two");
+        fs.writeFileSync("three.txt", "three");
     });
 
     teardown(function () {
@@ -20,49 +21,69 @@ suite("watcher", function() {
     });
 
     test("modify one file", function(done) {
-        sh.watcher("*.css").on("change", function(modified) {
+        this.slow(1000);
+        sh.watcher().watch("*.css", function() {
             this.close();
-            assert(modified.length === 1);
-            assert(modified[0] === pathModule.resolve("one.css"));
-            assert(fs.readFileSync(modified[0]).toString() === "modified");
             done();
         });
-        sh.writeFile("one.css", "modified");
+        sh.writeFile("one.css", "changed");
     });
 
     test("modify two files", function(done) {
-        sh.watcher("*.css").on("change", function(modified) {
+        this.slow(1000);
+        sh.watcher().watch("*.css", function() {
             this.close();
-            assert(modified.length === 2);
-            assert(modified[0] === pathModule.resolve("one.css"));
-            assert(modified[1] === pathModule.resolve("two.css"));
-            assert(fs.readFileSync(modified[0]).toString() === "modified one");
-            assert(fs.readFileSync(modified[1]).toString() === "modified two");
             done();
         });
         sh.writeFile("one.css", "modified one");
         sh.writeFile("two.css", "modified two");
     });
 
-    test("modify twice", function(done) {
-        this.slow(500);
+    test("not modified textfile", function(done) {
+        this.slow(2000);
+        var watcher = sh.watcher();
+        watcher.watch("*.txt", function() {
+            done();
+            watcher.close();
+        });
+        watcher.watch("*.css", function() {
+            throw new Error();
+        });
+        fs.writeFileSync("three.txt", "changed three");
+    });
 
-        var first = true;
-        sh.watcher("*.css", { delay: 0, interval: 0 }).on("change", function(modified) {
-            assert(modified.length === 1);
-            assert(modified[0] === pathModule.resolve("one.css"));
-
-            if(first) {
-                first = false;
-                return;
-            }
-            this.close();
+    test("modify in new directory", function(done) {
+        this.slow(2000);
+        var watcher = sh.watcher();
+        watcher.watch("newdir/*.css", function() {
             done();
         });
 
-        sh.writeFile("one.css", "once");
+        fs.mkdirSync("newdir");
         setTimeout(function() {
-            sh.writeFile("one.css", "twice");
-        }, 200);
+            fs.writeFileSync("newdir/foo.css", "newdir");
+        }, 600);
+    });
+
+    test("modify twice", function(done) {
+        this.slow(3000);
+        this.timeout(3000);
+        var first = true;
+        var count = 0;
+        var watcher = sh.watcher();
+        watcher.watch("*.css", function() {
+            count++;
+            if(first) {
+                first = false;
+            } else {
+                assert(count === 2);
+                done();
+            }
+        });
+
+        sh.writeFile("one.css", "modified one");
+        setTimeout(function() {
+            sh.writeFile("two.css", "modified two");
+        }, 1000);
     });
 });
